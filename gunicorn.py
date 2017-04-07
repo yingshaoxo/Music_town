8c9789dde94b923975ce561e7303e5c323858cce
yingshaoxo <yingshaoxo@gmail.com>
2017-04-07 15:48:43 +0800

    update

--- gunicorn.py
+++ gunicorn.py
@@ -1,11 +1,10 @@
 import time
 import os
 
-
+#rm nohup.out
 commands = '''
-rm nohup.out
 pkill gunicorn
-sudo nohup gunicorn -b 0.0.0.0:80 -w 4 -t 270 app:app &
+sudo nohup gunicorn -b 0.0.0.0:80 -w 4 -t 777 app:app &
 '''
 commands = [c for c in commands.split('\n') if c != '']
 for c in commands:
