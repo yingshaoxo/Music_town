02e10e803078f179ae9bdc8797c461c345821ac1
yingshaoxo <yingshaoxo@gmail.com>
2018-02-11 12:01:54 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 pkill python3.6
-python3.6 app/app.py &
+
+sudo apt-get install -y python3.6
+sudo apt-get install -y python3-pip
+sudo pip3 install --no-cache-dir -r requirements.txt
+
+nohup python3.6 app/app.py &
